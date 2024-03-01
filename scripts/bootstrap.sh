25c9bf65774fac2bf8d8705ef512d5236e12a123
Madh93 <mhdez@protonmail.com>
2024-03-01 15:51:17 +0100

    feat: add install and bootstrap scripts

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo "[Dotfiles] Bootstrapping..."
+
+# Setup config files
+ln -sf "$(readlink -f ../.tmux.conf)" $HOME
+ln -sf "$(readlink -f ../.vimrc)" $HOME
+ln -sf "$(readlink -f ../.zshrc)" $HOME
+
+# Finish bootstrap
+touch "$(readlink -f ../.bootstrapped)"
+
+echo "[Dotfiles] Bootstrapping. OK!"
---
+++ scripts/install.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# Installers
+install_arch_packages() {
+  sudo pacman -Sy --noconfirm vim tmux fzf
+}
+
+install_alpine_packages() {
+  sudo apk add --no-cache vim tmux fzf zsh-vcs ncurses
+}
+
+install_debian_packages() {
+  sudo apt update
+  sudo apt install -y vim tmux fzf
+}
+
+install_macos_packages() {
+  brew install vim tmux fzf
+}
+
+echo "[Dotfiles] Installing packages..."
+
+# Install packages
+if [ -f /etc/arch-release ] || [ -f /etc/manjaro-release ]; then
+  install_arch_packages
+elif [ -f /etc/alpine-release ]; then
+  install_alpine_packages
+elif [ -f /etc/debian_version ]; then
+  install_debian_packages
+elif [ "$(uname)" == "Darwin" ]; then
+  install_macos_packages
+else
+  echo "[Dotfiles] Unknown Operating System. Skipping packages installation..."
+fi
+
+# Install vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo "[Dotfiles] Installing packages. OK!"
+
+# Bootstrap
+if [ ! -f ../.bootstrapped ]; then
+  ./bootstrap.sh
+fi
